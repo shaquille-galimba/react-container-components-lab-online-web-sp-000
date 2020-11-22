035189c680b7f604084b5fc441cdd61bc826fa81
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-11-22 12:09:14 -0500

    commit tree b1b0e539ec3069ba0c1f36ad3023fd66e3f47efa

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,12 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({ reviews }) => (
+	<div className="review-list">
+		{reviews.map((review, idx) => <div className="review" key={idx}>
+			<a href={review.link.url} target="_blank">{review.display_title}</a>
+		</div>)}
+	</div>
+)
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,51 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+const NYT_API_KEY = 'VGbdIzuulPIL66CHip44p39AXyLwUQGk';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component{
+	constructor() {
+		super()
+
+		this.state = {
+			reviews: [],
+			searchTerm: ""
+		}
+	}
+
+	handleSubmit = event => {
+		event.preventDefault()
+
+		fetch(URL + `&query=${this.state.searchTerm}`)
+		.then(resp => resp.json())
+		.then(reviewData => this.setState({ reviews: reviewData.results }))
+		.catch(error => {
+			console.error('Error:', error)
+		})
+	}
+
+	handleChange = event => {
+		this.setState({
+			searchTerm: event.target.value
+		})
+	}
+
+	render() {
+		return (
+			<div className="searchable-movie-reviews">
+				<form onSubmit={this.handleSubmit}>
+					Search for movie reviews:
+					<input type="text" value={this.state.searchTerm} onChange={this.handleChange}/>
+				</form>
+				<div className="searchable-movie-reviews">
+					<MovieReviews reviews={this.state.reviews}/>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default SearchableMovieReviewsContainer
