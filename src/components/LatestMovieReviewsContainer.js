190d4b676d4ab51f7a7e31ae0a27ea0a5e4e0ddf
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-11-22 12:10:45 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,37 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+const NYT_API_KEY = 'VGbdIzuulPIL66CHip44p39AXyLwUQGk';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+	constructor() {
+		super()
+
+		this.state = {
+			reviews: []
+		}
+	}
+
+	componentDidMount() {
+		fetch(URL)
+		.then(resp => resp.json())
+		.then(reviewData => this.setState({ reviews: reviewData.results }))
+		.catch(error => {
+			console.error('Error:', error);
+		})
+	}
+
+	render() {
+		return (
+			<div className="latest-movie-reviews">
+				<h1>Latest reviews!</h1>
+				<MovieReviews reviews={this.state.reviews}/>
+			</div>
+		)
+	}
+}
+
+export default LatestMovieReviewsContainer
